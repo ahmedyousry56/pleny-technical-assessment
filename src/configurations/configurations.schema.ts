5ea9a22c9b1511f6deb26e01438c54fe1e2c9cbd
ahmedyousry56 <ahmedyousry098@gmail.com>
2026-05-21 14:55:00 +0300

    add default language code to example.env and config schema

--- src/configurations/configurations.schema.ts
+++ src/configurations/configurations.schema.ts
@@ -4,4 +4,5 @@ import * as Joi from 'joi';
 export const configurationsSchema = Joi.object({
     NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
     PORT: Joi.number().default(3000),
+    DEFAULT_LANGUAGE: Joi.string().valid('en', 'ar').default('en'),
 });
